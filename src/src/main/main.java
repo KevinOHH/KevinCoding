85278dc8171b30fbaf39e01e0cbead313fec8a8b
Kevin Hernández <KevinOHdezHdez@hotmail.com>
2025-04-24 19:25:19 -0600

    Antes de implemetar ventana de Agregar Cliente

---
+++ src/src/main/main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package src.main;
+
+/**
+ *
+ * @author Kevin
+ */
+public class main {
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+        principal.show();
+    }
+}
